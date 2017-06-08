abb11233f58a024749f3d3ab721a9193eb2d719e
C410 <C410@AceLabs.lagcc>
2017-06-08 15:16:21 -0400

    put a x at the end and failed to prepend

--- js/app.js
+++ js/app.js
@@ -1,6 +1,40 @@
 // console.log("this works");
 var userSubmit = document.querySelector('#user-submit');
+// ============================================
+// Create a "close" button and append it to each list item
 
+// var myNodelist = document.getElementsByTagName("LI");
+// var i;
+// for (i = 0; i < myNodelist.length; i++) {
+//   var span = document.createElement("SPAN");
+//   var txt = document.createTextNode("\u00D7");
+//   span.className = "close";
+//   span.appendChild(txt);
+//   myNodelist[i].appendChild(span);
+// }
+
+// // Click on a close button to hide the current list item
+// var close = document.getElementsByClassName("close");
+// var i;
+// for (i = 0; i < close.length; i++) {
+//   close[i].onclick = function() {
+//     var div = this.parentElement;
+//     div.style.display = "none";
+//   }
+// }
+
+
+
+
+
+
+
+
+
+// ========================================
+function prependChild(parent, newfirst){
+	parent.insertBefore(newfirst, parent.firstChild);
+}
 
 function addToDo(event){
 	event.preventDefault();
@@ -16,9 +50,24 @@ function addToDo(event){
 	// else if(UserInputVal = UserInputVal.indexOf(' ') >= 0){
 	// 	return false;
 	// }
-	toDoList.innerHTML += '<li>' + userInput.value + '</li>';
+	
+	var inputList = '<li>' + userInput.value  + '<span class ="close">' +  "\u00D7" + '</span>'  +  '</li>';
+	
+	
+	toDoList.innerHTML += inputList ;
 	userInput.value = '';
-
+	// ===================this is to close the x at the end=======================
+	var close = document.getElementsByClassName("close");
+	var i;
+	for (i = 0; i < close.length; i++) {
+	  close[i].onclick = function() {
+   	  var div = this.parentElement;
+   	  div.style.display = "none";
+  }
+}
 }
 
+// Click on a close button to hide the current list item
+
 userSubmit.addEventListener('click', addToDo, false);
+
